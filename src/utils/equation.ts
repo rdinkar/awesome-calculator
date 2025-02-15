72e86bf11b82035647efe6a9c0ff2a05e49f41c4
Rahul Dinkar <rahul.dinkar@synaptic.com>
2025-02-15 20:57:59 +0530

    equation evaluation fix

--- src/utils/equation.ts
+++ src/utils/equation.ts
@@ -8,7 +8,9 @@ export const evaluateEquation = (equation: string, x: number): number => {
   // Replace x^n with Math.pow(x,n)
   const powReplaced = equation.replace(/x\^(\d+)/g, `Math.pow(x,$1)`);
   // Replace x with the actual value
-  const xReplaced = powReplaced.replace(/x/g, x.toString());
+  const xReplaced = powReplaced
+    .replace(/(\d)x/g, `$1*x`)
+    .replace(/x/g, x.toString());
   try {
     // eslint-disable-next-line no-eval
     return eval(xReplaced);
